eab67cd2f160d2c6d7632578ee2a9e6cc6477407
falilp <78731447+falilp@users.noreply.github.com>
2023-03-08 17:54:08 +0100

    Update log.php

--- PW/Back/log.php
+++ PW/Back/log.php
@@ -1,21 +1,17 @@
 <?php 
 function iniciar($email,$contrasena){
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $email = $_POST["email"];
-        $contrasena = $_POST["contrasena"];
-        if(strlen($contrasena) >= 8){
-            $conexion = mysqli_connect("127.0.0.1","ADMIN","","kmb") or die("Conexion fallida");
-            $consulta = "SELECT email,contrasena FROM usuarios WHERE email = $email";
-            $resultado = mysqli_query($conexion,$consulta);
-            if($resultado){
-                if(password_verify($contrasena,$resultado["contrasena"])){
-                    header("Location:http://localhost/Main.php"); 
-                }
-            }else{
-                    echo "No se pudo iniciar al Usuario.";
+    if(strlen($contrasena) >= 8){
+        $conexion = mysqli_connect("127.0.0.1","ADMIN","","kmb") or die("Conexion fallida");
+        $consulta = "SELECT email,contrasena FROM usuarios WHERE email = $email";
+        $resultado = mysqli_query($conexion,$consulta);
+        if($resultado){
+            if(password_verify($contrasena,$resultado["contrasena"])){
+                header("Location:http://localhost/Main.php"); 
             }
-            mysqli_close($conexion);
+        }else{
+                echo "No se pudo iniciar al Usuario.";
         }
-    }
+        mysqli_close($conexion);
+    }    
 }
 ?>
