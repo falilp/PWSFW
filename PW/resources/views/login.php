641055cc3f112255c37359934f28e477b9e847d1
falilp <78731447+falilp@users.noreply.github.com>
2023-03-08 17:19:35 +0100

    Create login.php

--- PW/resources/views/login.php
+++ PW/resources/views/login.php
@@ -17,7 +17,18 @@
         if($_SERVER["REQUEST_METHOD"] == "POST"){
             $email = $_POST["email"];
             $contrasena = $_POST["contrasena"];
+            if(strlen($contrasena) >= 8){
+                $conexion = mysqli_connect("127.0.0.1","ADMIN","","kmb") or die("Conexion fallida");
+                $consulta = "SELECT email,contrasena FROM usuarios WHERE email = $email";
+                $resultado = mysqli_query($conexion,$consulta);
+                if($resultado){
+                    password_verify($contrasena,$resultado["contrasena"]);
+                }else{
+                    echo "No se pudo registrar al Usuario.";
+                }
+                mysqli_close($conexion);
+            }
         }
     ?>
     </body>
-</html1>
+</html>
