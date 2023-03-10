5cbddc1cc57528810a8ab8d60fc3efd4f980cc91
falilp <78731447+falilp@users.noreply.github.com>
2023-03-10 12:03:33 +0100

    Update reg.php

--- PW/Back/reg.php
+++ PW/Back/reg.php
@@ -3,7 +3,7 @@ function registrar($nombre,$apellidos,$email,$telefono,$contrasena){
     $pass = password_hash($contrasena,PASSWORD_BCRYPT,['cost' => 10]);
     if(strlen($telefono) == 9 && strlen($contrasena) >= 8){
         $conexion = mysqli_connect("127.0.0.1","ADMIN","","kmb") or die("Conexion fallida");
-        $consulta = "INSERT INTO usuarios (nombre,apellidos,email,telefono,contrasena) VALUES ('$nombre','$apellidos','$email','$telefono','$pass')";
+        $consulta = "INSERT INTO usuario (nombre,apellidos,email,telefono,contrasena) VALUES ('$nombre','$apellidos','$email','$telefono','$pass')";
         if(mysqli_query($conexion,$consulta)){
             echo "Usuario registrado.";
         }else{
