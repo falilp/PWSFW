415951bdf8f03208de71f7e6048a784f99cb5127
falilp <78731447+falilp@users.noreply.github.com>
2023-03-12 21:11:53 +0100

    Add files via upload

---
+++ PW/Back/regEvento.php
@@ -0,0 +1,13 @@
+<?php 
+function registrarEvento($descripcion,$fecha,$codpista){
+    $conexion = mysqli_connect("127.0.0.1","ADMIN","","kmb") or die("Conexion fallida");
+    $consulta = "INSERT INTO evento (FechaEvento,Descripcion,CodPista) VALUES ('$fecha','$descripcion','$codpista')";
+    //Falta modificar la pista para ponerla ocupada
+    if(mysqli_query($conexion,$consulta)){
+        echo "Evento registrada.";
+    }else{
+        echo "No se pudo registrar la Pista.";
+    }
+    mysqli_close($conexion);
+} 
+?>
