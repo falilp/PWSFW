25afaa165676d6ba13c91a66f7e2784e74f86703
falilp <falilealp@gmail.com>
2023-03-23 19:47:46 +0100

    merequetengue

--- PW/Back/regAlquiler.php
+++ PW/Back/regAlquiler.php
@@ -8,13 +8,14 @@ function registrarAlquiler($codigoPista,$fecha,$sesion){
     $resultado = $conexion->query($consultaU);
     $usuario = $resultado->fetch_array();
 
-    $consultaP = "SELECT * FROM usuario WHERE codPista = '$codigoPista'";
+    $consultaP = "SELECT * FROM pista WHERE codPista = '$codigoPista'";
     $resultado = $conexion->query($consultaP);
     $pista = $resultado->fetch_array();
 
-    $consulta = "INSERT INTO alquiler (codPista,codUsuario,fecha_alquiler,precio,Descuento) VALUES ('$codigoPista','$usuario[0]','$fecha','$pista[2]','0')";
-    
-    if(mysqli_query($conexion,$consulta)){
+    $insert = "INSERT INTO alquiler (codPista,codUsuario,fecha_alquiler,precio,Descuento) VALUES ('$codigoPista','$usuario[0]','$fecha','$pista[2]','0')";
+    $update = "UPDATE pista SET disponible = 1 WHERE codPista = '$codigoPista'";
+
+    if($conexion->query($insert) && $conexion->query($update)){
         header("Location:http://localhost/PWSFW/PW/resources/views/Indice.php"); 
     }else{
         header("Location:http://localhost/PWSFW/PW/resources/views/paginaERROR.html");
@@ -25,7 +26,7 @@ function registrarAlquiler($codigoPista,$fecha,$sesion){
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $codigoPista = $_POST["codep"];
-    $codigoPista = $_POST["fecha"];
+    $fecha = $_POST["fecha"];
     $sesion = new Sesion();
 
     registrarAlquiler($codigoPista,$fecha,$sesion);
