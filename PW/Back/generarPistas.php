6a995ecb1ccd449efa9f46e8f225d902ff9bee50
falilp <falilealp@gmail.com>
2023-03-24 12:54:34 +0100

    cancelar

---
+++ PW/Back/generarPistas.php
@@ -0,0 +1,14 @@
+<?php
+function(){
+    $conexion = mysqli_connect("127.0.0.1","ADMIN","","kmb") or die("Conexion fallida");
+    $dicc = array(1=>"Pista de fútbol 11",2=>"Pista de fútbol 7",3=>"Pista de fútbol sala",4=>"Pista de tenis",5=>"Pista de baloncesto",6=>"Pista de voleibol",7=>"Pista de pádel");
+    $horas = array(1=>"",2=>"Pista de fútbol 7",3=>"Pista de fútbol sala",4=>"Pista de tenis",5=>"Pista de baloncesto",6=>"Pista de voleibol",7=>"Pista de pádel",8=>"");
+
+    for(){
+        for(){
+
+        }
+    }
+}
+
+?>
